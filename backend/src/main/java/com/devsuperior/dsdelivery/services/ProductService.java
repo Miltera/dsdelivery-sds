6110b4cee82cc7fab03f849375e2eda610ad0070
Milton Rodrigues Jr <miltonrodrigues.jr@hotmail.com>
2021-01-06 10:14:12 -0800

    Created Route Orders (POST)

--- backend/src/main/java/com/devsuperior/dsdelivery/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/services/ProductService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdelivery.dto.ProductDTO;
 import com.devsuperior.dsdelivery.entities.Product;
